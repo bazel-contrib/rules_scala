942834212c467b87e1064da46689e98512e8c2a9
Frank Portman <frank1214@gmail.com>
2025-11-24 16:22:39 -0500

    tmp

---
+++ test/toolchain_plugins/SimplePluginTest.scala
@@ -0,0 +1,9 @@
+package test.toolchain_plugins
+
+// Simple test that doesn't require any special plugin features
+// This verifies the basic compilation works
+object SimplePluginTest {
+  def test(): String = {
+    "Global plugin mechanism works"
+  }
+}
