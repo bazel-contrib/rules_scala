39114d9d90119282fbf03c8af06be67af50c454d
Frank Portman <frank1214@gmail.com>
2025-11-24 20:01:36 -0500

    remove junk

--- test/toolchain_plugins/test_without_toolchain.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Test that verifies the global plugin is actually being used
-
-set -e
-echo "Testing that simple compilation works WITHOUT the special toolchain..."
-bazel build //test/toolchain_plugins:test_simple
-
-echo "Test passed - simple compilation works without special toolchain"
