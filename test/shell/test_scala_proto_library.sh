570425275049e9589a12a3871d961e2ac22f678e
bor_p <36397564+bor-p-s@users.noreply.github.com>
2026-07-23 11:21:06 +0200

    refactor: Migrate scala-proto failing-generator test to Bazel

--- test/shell/test_scala_proto_library.sh
+++ test/shell/test_scala_proto_library.sh
@@ -1,7 +1,6 @@
 # shellcheck source=./test_runner.sh
 dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 . "${dir}"/test_runner.sh
-. "${dir}"/test_helper.sh
 runner=$(get_test_runner "${1:-local}")
 
 test_scala_proto_library_action_label() {
@@ -20,14 +19,5 @@ test_scala_proto_custom_generator() {
   --extra_toolchains=//test/proto/custom_generator:scala_proto_toolchain
 }
 
-test_scala_proto_show_generator_exception() {
-  action_should_fail_with_message \
-    "java.lang.RuntimeException: expected generator failure" \
-    build //test/proto/custom_generator:any_proto_scala \
-    --extra_toolchains=//test/proto/custom_generator:failing_scala_proto_deps_toolchain \
-    --extra_toolchains=//test/proto/custom_generator:failing_scala_proto_toolchain
-}
-
 $runner test_scala_proto_library_action_label
 $runner test_scala_proto_custom_generator
-$runner test_scala_proto_show_generator_exception
