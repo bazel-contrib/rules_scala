52e7366a6689eb443696623ec1748ad13016b1c7
bor_p <36397564+bor-p-s@users.noreply.github.com>
2026-08-24 14:43:49 +0200

    refactor: Migrate test_scala_import_source_jar.sh to Bazel-native sh_test targets

--- test_rules_scala.sh
+++ test_rules_scala.sh
@@ -32,6 +32,5 @@ $runner bazel build //test_statsfile:SimpleNoStatsFile_statsfile --extra_toolcha
 . "${test_dir}"/test_misc.sh
 . "${test_dir}"/test_scalafmt.sh
 . "${test_dir}"/test_scala_binary.sh
-. "${test_dir}"/test_scala_import_source_jar.sh
 . "${test_dir}"/test_scala_library.sh
 . "${test_dir}"/test_semanticdb.sh
