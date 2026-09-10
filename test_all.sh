5eae3d61eed37a46f93b97d5764d8c21c582f4f1
bor_p <36397564+bor-p-s@users.noreply.github.com>
2026-09-10 18:34:00 +0200

    test: Migrate test_thirdparty_version.sh to native Bazel targets (#1974)

--- test_all.sh
+++ test_all.sh
@@ -12,5 +12,5 @@ dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 #"${dir}"/test_intellij_aspect.sh
 "${dir}"/test_examples.sh
 "${dir}"/test_coverage.sh
-"${dir}"/test_thirdparty_version.sh
+bazel test --test_output=errors -- //test_thirdparty_version/...
 "${dir}"/dt_patches/dt_patch_test.sh
